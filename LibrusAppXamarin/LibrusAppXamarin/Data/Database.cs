fddd63845966f0df2c2f96d0b4ad3d5ad7286552
uczen <uczen@mail.com>
2024-01-19 11:44:33 +0100

    dodanie pełnego kodu klasy Database, zmiany w App.cs oraz dodanie klas User, Score, Subject z ich właściwościami

--- LibrusAppXamarin/LibrusAppXamarin/Data/Database.cs
+++ LibrusAppXamarin/LibrusAppXamarin/Data/Database.cs
@@ -9,16 +9,53 @@ namespace LibrusAppXamarin.Data
 {
     public class Database
     {
-        public readonly SQLiteAsyncConnection _connection;
-        public Database(string dbPath)
+        readonly SQLiteAsyncConnection _database;
+        public Database(string db)
         {
-            _connection = new SQLiteAsyncConnection(dbPath);
-            _connection.CreateTableAsync<Person>().Wait();
+            _database = new SQLiteAsyncConnection(db);
+            _database.CreateTableAsync<User>().Wait();
+            _database.CreateTableAsync<Score>().Wait();
+            _database.CreateTableAsync<Subject>().Wait();
         }
 
-        public Task<List<Person>> GetPersonAsync(Person person)
+        public Task<int> InsertUser(User user)
         {
-            return _connection.Table<Person>().ToListAsync();
+            return _database.InsertAsync(user);
+        }
+
+        public Task<List<User>> GetUsers()
+        {
+            return _database.QueryAsync<User>("SELECT * FROM User");
+        }
+
+        public Task<List<User>> GetUserFilter(string login, string password)
+        {
+            return _database.QueryAsync<User>("SELECT * FROM User WHERE Login=? AND Password=?", login, password);
+        }
+
+        public Task<List<Subject>> GetSubjects()
+        {
+            return _database.QueryAsync<Subject>("SELECT * FROM Subject");
+        }
+
+        public Task<List<Score>> GetScories()
+        {
+            return _database.QueryAsync<Score>("SELECT * FROM Score");
+        }
+
+        public Task<int> InsertSubject(Subject subject)
+        {
+            return _database.InsertAsync(subject);
+        }
+
+        public Task<int> InsertScore(Score score)
+        {
+            return _database.InsertAsync(score);
+        }
+
+        public Task<List<Score>> GetScories(int user_id, int subject_id, string period)
+        {
+            return _database.QueryAsync<Score>("SELECT * FROM Score WHERE User_id=? AND Subject_id=? AND Period=?", user_id, subject_id, period);
         }
     }
 }
