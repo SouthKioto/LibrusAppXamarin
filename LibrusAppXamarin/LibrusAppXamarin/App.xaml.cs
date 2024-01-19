398fd796a34d7307e7ac14fffee47415e5f1ba10
uczen <uczen@mail.com>
2024-01-19 11:44:33 +0100

    dodanie pełnego kodu klasy Database, zmiany w App.cs oraz dodanie klas User, Score, Subject z ich właściwościami

--- LibrusAppXamarin/LibrusAppXamarin/App.xaml.cs
+++ LibrusAppXamarin/LibrusAppXamarin/App.xaml.cs
@@ -1,11 +1,29 @@
-﻿using System;
+﻿using LibrusAppXamarin.Data;
+using System;
+using System.IO;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
 
 namespace LibrusAppXamarin
 {
+    
+
     public partial class App : Application
     {
+        static Database database;
+
+        public static Database Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new Database(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "librusApp.db3"));
+                }
+                return database;
+            }
+        }
+
         public App()
         {
             InitializeComponent();
